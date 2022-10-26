2a535c9761a4eaa59e0fc364b2d5e44c13afcc56
mibbim <michele.berti.g@gmail.com>
2022-10-26 17:07:09 +0200

    Added logger, set variable seed in build_dataset, set guroby logging to be more elegant

--- experiments/instance_size/experiment.py
+++ experiments/instance_size/experiment.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import sys
 from datetime import datetime
 from pathlib import Path
 
@@ -26,8 +29,8 @@ all_optimizations = [
 ]
 
 
-def build_datasets(instances: int = 5, n: int = 30, ) -> NPA:
-    rng = np.random.default_rng(42)
+def build_datasets(instances: int = 5, n: int = 30, seed: int = 42) -> NPA:
+    rng = np.random.default_rng(seed)
     dims = 2
     datasets = 4 * rng.random((instances, n, dims)) + 1
     return datasets
@@ -48,20 +51,16 @@ def measure(obj, rotation, rho, N, d, time_threshold, ):
               radius=r,
               rotation=rotation,
               optimizations=all_optimizations)
-    cpp._model.Params.LogFile = "{now:%H:%M:%S}.log".format(
-        now=datetime.now())
+    now = datetime.now()
+    os.makedirs("{now:%d-%m}/guroby".format(now=now), exist_ok=True)
+    cpp._model.Params.LogFile = "{now:%d-%m}/guroby/{now:%H:%M:%S}.log".format(now=now)
     cpp._model.Params.LogToConsole = 0
-    time = cpp.optimize(1000, 1000, time_limit=time_threshold)
+    time = cpp.optimize(1000, 1000, time_limit=time_threshold, plot=False, show=False)
     area = cpp.area
     count = cpp.count
     obj_val = cpp.obj_val
-    print(
-        f"{obj} problem with {rotation=}, {rho=}, {N=}, {time=}, {area=}, {count=}, {obj_val=}",
-    )
-    with open('summary.txt', 'a') as summary_file:
-        print(
-            f"{obj} problem with {rotation=}, {rho=}, {N=}, {time=}, {area=}, {count=}, {obj_val=}",
-            file=summary_file)
+    logger.info(f"{obj} problem with "
+                f"{rotation=}, {rho=}, {N=}, {time=}, {area=}, {count=}, {obj_val=}")
 
 
 def main():
@@ -70,17 +69,39 @@ def main():
     for obj in ["count", "volume"]:
         for rotation in (True, False):
             for rho in (0.2, 0.4, 0.6, 0.8, 1):
-                for N in np.arange(18, 30 + 1, 5):
+                for N in np.arange(10, 18, 1):
                     for i, d in enumerate(datasets):
                         try:
                             measure(obj, rotation, rho, N, d, time_threshold)
-                        except RuntimeError:
+                        except AttributeError:
+                            print(
+                                f"Attribute Error Occured at {obj=} {rotation=}, {rho=}, {N=}   {i=}")
                             continue
 
         pass
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(
+        logging.Formatter("[%(asctime)s][%(name)s %(lineno)d][%(levelname)s] %(message)s",
+                          datefmt="%Y-%m-%d %H:%M:%S")
+    )
+    logger.addHandler(stdout_handler)
+
+    logfile_handler = logging.FileHandler("train.log")
+    logfile_handler.setFormatter(
+        logging.Formatter(
+            "[%(asctime)s][%(module)s %(lineno)d][%(levelname)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+    )
+    logger.addHandler(logfile_handler)
+
+    # print(isinstance(AttributeError, RuntimeError))
     # for d in build_datasets():
     #     measure(obj="count", rotation=True, rho=0.2, N=15, d=d, time_threshold=hour)
     main()
